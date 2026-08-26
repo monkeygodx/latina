fb03e8bd4a007d3004654d595f8014cf226a28c8
monkeygodx <kinfolk651@gmail.com>
2026-08-25 23:32:07 -0500

    Create server.js

---
+++ server.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const path    = require('path');
+
+const app  = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(express.static(path.join(__dirname, 'public'), { extensions: ['html'] }));
+
+app.listen(PORT, () => console.log(`latinagod.xyz running on port ${PORT}`));
